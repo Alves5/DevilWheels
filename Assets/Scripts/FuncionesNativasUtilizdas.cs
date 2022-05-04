4b42e64a7d78a5afa5763b20e435ef235bac3844
Daniel Alves <76122600+Alves5@users.noreply.github.com>
2022-05-03 22:23:06 -0300

    Add files via upload

---
+++ Assets/Scripts/FuncionesNativasUtilizdas.cs
@@ -0,0 +1,39 @@
+
+
+//Funcion que se usa para 
+void Awake ();
+
+void Start();
+
+void Update();
+
+void OnTriggerEnter2D(Collider2D cInfo);
+
+void OnTriggerExit2D(Collider2D cInfo);
+
+void OnMouseDown();
+
+void OnMouseExit();
+
+void OnMouseUp();
+
+StartCoroutine(CargoEscena());
+IEnumerator CargoEscena();
+
+void OnCollisionEnter2D(Collision2D cInfo);
+
+void OnCollisionExit2D(Collision2D cInfo);
+
+void FixedUpdate();
+
+masTiempo.CrossFadeAlpha(0,0,false);
+
+Instantiate();
+
+InvokeRepeating();
+
+CancelInvoke();
+
+Translate();
+
+Find ();
